7a8bacbc5e6951daa1c801b1e68995eba808ba59
santiagodev28 <shurtado308@gmail.com>
2026-02-10 18:51:52 -0500

    feat(bootstrap): finalize module and main configuration to run server

--- backend/src/application/use-cases/auth/validate-user/validate-user.use-case.ts
+++ backend/src/application/use-cases/auth/validate-user/validate-user.use-case.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import type { IUserRepository } from '../../../../domain/repositories';
 import { User } from '../../../../domain/entities';
+import { USER_REPOSITORY } from 'src/domain/repositories/user-repository.token';
 
 @Injectable()
 export class ValidateUserUseCase {
   constructor(
+    @Inject(USER_REPOSITORY)
     private readonly userRepository: IUserRepository,
   ) {}
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './infrastructure/filters/global-exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORS
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
+  // Validación global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Filtro de excepciones global
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+  await app.listen(3000);
+  console.log('Server running on http://localhost:3000');
 }
 bootstrap();
