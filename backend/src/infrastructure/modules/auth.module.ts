d9458e3cebc896a33bba9649cf8cf4a118f04026
santiagodev28 <shurtado308@gmail.com>
2026-02-17 23:10:04 -0500

    feature(database-config): Postgres database config in infrastructure

--- backend/src/infrastructure/modules/auth.module.ts
+++ backend/src/infrastructure/modules/auth.module.ts
@@ -4,12 +4,14 @@ import { LoginUseCase } from '../../application/use-cases/auth/login';
 import { ValidateUserUseCase } from '../../application/use-cases/auth/validate-user';
 import { UserMockRepository } from '../repositories';
 import { DI_TOKENS } from '../tokens/di.tokens';
+import { PrismaService } from '../database/primsa.service';
 
 @Module({
   controllers: [AuthController],
   
   providers: [
     // Use Cases
+    PrismaService,
     LoginUseCase,
     ValidateUserUseCase,
     
